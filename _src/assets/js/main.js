002a99c71bb0d2c4bdaf562eb3ababf9b2c74e59
arijdb <ariane.jurado@gmail.com>
2019-04-08 09:15:10 +0200

    funtions not working

---
+++ _src/assets/js/main.js
@@ -0,0 +1,46 @@
+
+'use strict';
+  //constantes de elementos:
+  const counter = document.querySelector('.counter');
+  const input = document.querySelector('.input');
+  const btn = document.querySelector('.btn');
+  const feedback = document.querySelector('.feedback');
+
+//Funcion condiconal
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+    
+  }
+  console.log('> ' + getRandomNumber(100));
+
+
+  function final() {
+    if (input.value < getRandomNumber) {
+    feedback.innerHTML = 'Demasiado bajo.'
+}
+  else if (input.value > getRandomNumber) {
+    feedback.innerHTML = 'Demasiado alto.'
+
+  } else if (input.value === getRandomNumber) {
+    feedback.innerHTML = '¡HAS GANADO, CAMPEONA!'
+  }
+  }
+  btn.addEventListener('click', final);
+
+  
+  //PIntar en azul: pistas feeback
+  //si input es > que getRandomNumber: "demasiado alto"
+  //si input es < que getRandomNumber: "demasiado bajo"
+  //si input es === que getRandomNumber: "¡HAS GANADO, CAMPEONA!" TERMINA(no se como termina) creo que siendo el ultimo return
+
+
+
+//condicional if
+//funcion (condicional dentro de funcion?????)
+
+//listener de click de boton de prueba
+
+
+
