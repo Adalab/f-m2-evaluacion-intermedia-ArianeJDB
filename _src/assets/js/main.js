2363f646211d55411610416e80f62460038813ad
arijdb <ariane.jurado@gmail.com>
2019-04-08 09:39:59 +0200

    finish time

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,43 +6,25 @@
   const btn = document.querySelector('.btn');
   const feedback = document.querySelector('.feedback');
 
-
-
-  function final() {
-    if (input.value < getRandomNumber) {
-    feedback.innerHTML = 'Demasiado bajo.'
-}
-  else if (input.value > getRandomNumber) {
-    feedback.innerHTML = 'Demasiado alto.'
-
-  } else if (input.value === getRandomNumber) {
-    feedback.innerHTML = '¡HAS GANADO, CAMPEONA!'
-  }
-  }
-
-  btn.addEventListener('click', final);
-  
-function getRandomNumber(max) {
+  function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
     
   }
 
-
   console.log('> ' + getRandomNumber(100));
   
 
+  function final() {
+    if (input.value < getRandomNumber(100)) {
+    feedback.innerHTML = 'Demasiado bajo.'
+  }
+  else if (input.value > getRandomNumber(100)) {
+    feedback.innerHTML = 'Demasiado alto.'
+
+  } else if (input.value === getRandomNumber(100)) {
+    feedback.innerHTML = '¡HAS GANADO, CAMPEONA!'
+  }
+  }
   
-  //PIntar en azul: pistas feeback
-  //si input es > que getRandomNumber: "demasiado alto"
-  //si input es < que getRandomNumber: "demasiado bajo"
-  //si input es === que getRandomNumber: "¡HAS GANADO, CAMPEONA!" TERMINA(no se como termina) creo que siendo el ultimo return
-
-
-
-//condicional if
-//funcion (condicional dentro de funcion?????)
-
-//listener de click de boton de prueba
-
-
+  btn.addEventListener('click', final);
 
